e75be07e50701f4c6b7fdbfeb8ce274ec260611b
amaury2409 <amaury.henrottay@gmail.com>
2019-11-25 20:26:03 +0100

    try/catch

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,32 @@
 #!/usr/bin/env node
-console.log('yo man');
+const countryList = require('country-list');
+const axios = require('axios');
+
+
+const { getCode, getName } = require('country-list');
+const country = process.argv[2];
+countryCode = (getCode(country));
+console.log(getCode(country));
+
+let currentDay = new Date();
+let currentYear = currentDay.getFullYear();
+if (country == null) {
+    console.log("please use a country name as parameters");
+} else if (countryCode == undefined) {
+    console.log('this country is not available');
+    
+} else {
+    getholidays();
+}
+
+
+
+
+async function getholidays() {
+  try {
+    const response = await axios.get("https://date.nager.at/Api/v2/PublicHolidays/" + currentYear + "/" + countryCode);
+    console.log(response.data[0]);
+  } catch (error) {
+    console.error(error);
+  }
+}
