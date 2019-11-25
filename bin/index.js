d1b4ff9cd3bd19184bc3ba8eb333b777d75ceea6
amaury2409 <amaury.henrottay@gmail.com>
2019-11-25 21:50:58 +0100

    testing ora

--- bin/index.js
+++ bin/index.js
@@ -4,15 +4,17 @@ const axios = require('axios');
 const chalk = require('chalk');
 const { getCode, getName } = require('country-list');
 
+
+
 let args = process.argv.slice(2);
 let country = args[0] || 'Belgium';
 let currentYear = args[1] || new Date().getFullYear();
 let countryCode = (getCode(country));
 
 if (countryCode == undefined) {
-    console.log(chalk.red('Sorry, this country is not available !'));
+   console.log(chalk.red('Sorry, this country is not available !'));
 } else {
-    console.log(chalk.green("Holidates in "+ chalk.underline.bold(country)  + " for the year " + chalk.underline.bold(currentYear) + ":\n"));
+   console.log(chalk.green("Holidates in "+ chalk.underline.bold(country)  + " for the year " + chalk.underline.bold(currentYear) + ":\n"));
     getholidays();
 }
 
