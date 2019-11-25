24d97253bab7e30dc31e942f2cffd279ac2f158c
amaury2409 <amaury.henrottay@gmail.com>
2019-11-25 21:13:41 +0100

    check

--- bin/index.js
+++ bin/index.js
@@ -8,17 +8,20 @@ let country = args[0] || 'Belgium';
 let currentYear = args[1] || new Date().getFullYear();
 let countryCode = (getCode(country));
 
-
 if (countryCode == undefined) {
     console.log('this country is not available');
 } else {
+    console.log("Holidates in "+ country + " for the year " + currentYear + ":\n");
     getholidays();
 }
 
 async function getholidays() {
   try {
     const response = await axios.get("https://date.nager.at/Api/v2/PublicHolidays/" + currentYear + "/" + countryCode);
-    console.log(response.data[0]);
+  
+    for (const item of response.data) {
+          console.log(item.date + " : " + item.name);
+    }
   } catch (error) {
     console.error(error);
   }
