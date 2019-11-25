f3bc62e59ccaa8a536cee70ba53af2e269314d13
amaury2409 <amaury.henrottay@gmail.com>
2019-11-25 20:52:47 +0100

    slice arg

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,13 @@
 #!/usr/bin/env node
 const countryList = require('country-list');
 const axios = require('axios');
-
-
 const { getCode, getName } = require('country-list');
-const country = process.argv[2];
-countryCode = (getCode(country));
-console.log(getCode(country));
 
-let currentDay = new Date();
-let currentYear = currentDay.getFullYear();
+let args = process.argv.slice(2);
+let country = args[0] || 'Belgium';
+let currentYear = args[1] || new Date().getFullYear();
+let countryCode = (getCode(country));
+
 
 if (countryCode == undefined) {
     console.log('this country is not available');
@@ -17,11 +15,10 @@ if (countryCode == undefined) {
     getholidays();
 }
 
-
 async function getholidays() {
   try {
     const response = await axios.get("https://date.nager.at/Api/v2/PublicHolidays/" + currentYear + "/" + countryCode);
-    console.log(response.data);
+    console.log(response.data[0]);
   } catch (error) {
     console.error(error);
   }
